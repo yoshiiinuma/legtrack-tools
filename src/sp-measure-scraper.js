0dee8bd2f05a48c558e22fc19492e7c77da1159f
yoshiiinuma <iinumayoshiaki@gmail.com>
2020-07-10 15:18:00 -1000

    Separate SpMeasureScrape#scrape function into two

--- src/sp-measure-scraper.js
+++ src/sp-measure-scraper.js
@@ -21,7 +21,7 @@ const TYPE = 'sp';
 const JobStatus = ENUM.JobStatus;
 const DataType = ENUM.DataType;
 
-const scrape = (year, session, dir = '') => {
+const scrapeOld = (year, session, dir = '') => {
   const scrapeJob = ScrapeJob.create(nodeEnv);
   const type = TYPE + session.toLowerCase();
   let r = scrapeJob.insertJob(DataType.SPECIAL_SESSION);
@@ -44,25 +44,69 @@ const scrape = (year, session, dir = '') => {
       scrapeJob.updateJob(jobId, JobStatus.completed, total, updated);
       scrapeJob.insertDetail(jobId, typeId, JobStatus.completed, startedAt, total, updated);
       msg = 'completed';
-      Logger.info(`FetchSpMeasures: COMPLETED Total ${total}, Updated ${updated}`);
+      Logger.info(`SpMeasureScraper: COMPLETED Total ${total}, Updated ${updated}`);
     } else {
       scrapeJob.updateJob(jobId, JobStatus.skipped, 0, 0);
       msg = 'skipped';
-      Logger.info('FetchSpMeasures: SKIPPED');
+      Logger.info('SpMeasureScraper: SKIPPED');
     }
   } catch (e) {
       scrapeJob.updateJob(jobId, JobStatus.failed, 0, 0);
       msg = 'failed';
-      Logger.error('FetchSpMeasures: FAILED');
+      Logger.error('SpMeasureScraper: FAILED');
       Logger.error(e.toString());
       Logger.error(e.stack);
   }
   return { msg, total, updated };
 }
 
+const scrape = (year, session, dir = '') => {
+  const type = TYPE + session.toLowerCase();
+  const html = Fetcher.fetchHtml(SpMeasureScraper.getUrl(year, session));
+
+  if (!LocalFile.isChanged(html, year, type, dir)) {
+    Logger.info('SpMeasureScraper#scrape: SKIPPED');
+    return { msg: 'skipped', total: 0, updated: 0 };
+  }
+
+  LocalFile.save(html, year, type, dir);
+  const data = parseSpBills(html);
+  const localDB = SpMeasure.create(nodeEnv);
+  const r = localDB.bulkUpsert(data);
+  const updated = r.inserted + r.updated;
+  const total = r.ignore + updated; 
+  Logger.info(`SpMeasureScraper#scrape: COMPLETED Total ${total}, Updated ${updated}`);
+  return { msg: 'completed', total, updated };
+}
+
+const run = (year, session, dir = '') => {
+  const scrapeJob = ScrapeJob.create(nodeEnv);
+  const type = TYPE + session.toLowerCase();
+  const typeId = ENUM.MeasureType[type];
+  const job = scrapeJob.insertJob(DataType.SPECIAL_SESSION);
+  const jobId = job.lastInsertRowid;
+  const startedAt = job.startedAt;
+
+  try {
+    const r = SpMeasureScraper.scrape(year, session, dir);
+
+    if (r.msg === 'completed') {
+      scrapeJob.updateJob(jobId, JobStatus.completed, r.total, r.updated);
+    } else {
+      scrapeJob.updateJob(jobId, JobStatus.skipped, 0, 0);
+    } 
+    return r;
+  } catch (e) {
+    scrapeJob.updateJob(jobId, JobStatus.failed, 0, 0);
+    return { msg: 'failed', total: 0, updated: 0 };
+  }
+}
+
 const SpMeasureScraper = {
   getUrl,
-  scrape
+  scrape,
+  scrapeOld,
+  run
 };
 
 export default SpMeasureScraper;
--- test/sp-measure-scraper-test.js
+++ test/sp-measure-scraper-test.js
@@ -4,7 +4,7 @@ import sinon from 'sinon';
 import fs from 'fs';
 
 //import { fetchSpMeasures, getUrl } from '../src/fetch-sp-measures.js';
-import Scraper from '../src/sp-measure-scraper.js';
+import SpMeasureScraper from '../src/sp-measure-scraper.js';
 import Fetcher from '../src/fetcher.js';
 import LocalFile from '../src/local-file.js';
 import SpMeasure from '../src/local-sp-measure.js';
@@ -19,19 +19,18 @@ const HTML = fs.readFileSync(testFile, "utf8");
 const spBill = SpMeasure.create('test');
 const scrapeJob = ScrapeJob.create('test');
 
-let fetchHtml;
-let isChanged;
-let save;
-
-describe('Scraper#getUrl', () => {
+describe('SpMeasureScraper#getUrl', () => {
   it('returns speciall session bill url', () => {
-    const r = Scraper.getUrl(2020, 'a');
+    const r = SpMeasureScraper.getUrl(2020, 'a');
     expect(r).to.equal('https://www.capitol.hawaii.gov/splsession.aspx?year=2020a');
   });
 });
 
-describe('Scraper#scrape', () => {
+describe('SpMeasureScraper#scrapeOld', () => {
   const url = 'https://www.capitol.hawaii.gov/splsession.aspx?year=9999a';
+  let fetchHtml;
+  let isChanged;
+  let save;
 
   beforeEach(async () => {
     await scrapeJob.deleteAllDetails(); 
@@ -39,16 +38,16 @@ describe('Scraper#scrape', () => {
   });
 
   context('when fetch fails', () => {
-    before(() => {
+    beforeEach(() => {
       fetchHtml = sinon.stub(Fetcher, 'fetchHtml').throws();
     });
 
-    after(() => {
+    afterEach(() => {
       fetchHtml.restore();
     });
 
     it('returns failed as the message', async () => {
-      let r = await Scraper.scrape(year, session, dir);
+      let r = await SpMeasureScraper.scrapeOld(year, session, dir);
       expect(r).to.eql({ msg: 'failed', total: 0, updated: 0 });
       expect(fetchHtml.calledWith(url)).to.be.true;
 
@@ -66,21 +65,21 @@ describe('Scraper#scrape', () => {
   });
 
   context('when the fetched page is updated', () => {
-    before(async () => {
+    beforeEach(async () => {
       await spBill.deleteAll();
       fetchHtml = sinon.stub(Fetcher, 'fetchHtml').returns(HTML);
       isChanged = sinon.stub(LocalFile, 'isChanged').returns(true);
       save = sinon.stub(LocalFile, 'save');
     });
 
-    after(() => {
+    afterEach(() => {
       fetchHtml.restore();
       isChanged.restore();
       save.restore();
     });
 
     it('returns completed as the message', async () => {
-      let r = await Scraper.scrape(year, session, dir);
+      let r = await SpMeasureScraper.scrapeOld(year, session, dir);
       expect(r).to.eql({ msg: 'completed', total: 3, updated: 3 });
       expect(fetchHtml.calledWith(url)).to.be.true;
       expect(isChanged.calledWith(HTML, year, 'spa', dir)).to.be.true;
@@ -113,22 +112,21 @@ describe('Scraper#scrape', () => {
     });
   });
 
-
   context('when the fetched page is not updated', () => {
-    before(() => {
+    beforeEach(() => {
       fetchHtml = sinon.stub(Fetcher, 'fetchHtml').returns(HTML);
       isChanged = sinon.stub(LocalFile, 'isChanged').returns(false);
       save = sinon.stub(LocalFile, 'save');
     });
 
-    after(() => {
+    afterEach(() => {
       fetchHtml.restore();
       isChanged.restore();
       save.restore();
     });
 
     it('returns skipped as the message', async () => {
-      let r = await Scraper.scrape(year, session, dir);
+      let r = await SpMeasureScraper.scrapeOld(year, session, dir);
       expect(r).to.eql({ msg: 'skipped', total: 0, updated: 0 });
       expect(fetchHtml.calledWith(url)).to.be.true;
       expect(isChanged.calledWith(HTML, year, 'spa', dir)).to.be.true;
@@ -145,6 +143,148 @@ describe('Scraper#scrape', () => {
 
       r = await scrapeJob.countDetails(); 
       expect(r['COUNT(*)']).to.equal(0);
+    });
+  });
+});
+
+describe('SpMeasureScraper#scrape', () => {
+  const url = 'https://www.capitol.hawaii.gov/splsession.aspx?year=9999a';
+  let fetchHtml;
+  let isChanged;
+  let save;
+
+  context('when the fetched page is updated', () => {
+    beforeEach(async () => {
+      await spBill.deleteAll();
+      fetchHtml = sinon.stub(Fetcher, 'fetchHtml').returns(HTML);
+      isChanged = sinon.stub(LocalFile, 'isChanged').returns(true);
+      save = sinon.stub(LocalFile, 'save');
+    });
+
+    afterEach(() => {
+      fetchHtml.restore();
+      isChanged.restore();
+      save.restore();
+    });
+
+    it('returns completed as the message', async () => {
+      let r = await SpMeasureScraper.scrape(year, session, dir);
+      expect(r).to.eql({ msg: 'completed', total: 3, updated: 3 });
+      expect(fetchHtml.calledWith(url)).to.be.true;
+      expect(isChanged.calledWith(HTML, year, 'spa', dir)).to.be.true;
+      expect(save.calledWith(HTML, year, 'spa', dir)).to.be.true;
+
+      r = await spBill.count();
+      expect(r['COUNT(*)']).to.equal(3);
+    });
+  });
+
+  context('when the fetched page is not updated', () => {
+    beforeEach(() => {
+      fetchHtml = sinon.stub(Fetcher, 'fetchHtml').returns(HTML);
+      isChanged = sinon.stub(LocalFile, 'isChanged').returns(false);
+      save = sinon.stub(LocalFile, 'save');
+    });
+
+    afterEach(() => {
+      fetchHtml.restore();
+      isChanged.restore();
+      save.restore();
+    });
+
+    it('returns skipped as the message', async () => {
+      let r = await SpMeasureScraper.scrape(year, session, dir);
+      expect(r).to.eql({ msg: 'skipped', total: 0, updated: 0 });
+      expect(fetchHtml.calledWith(url)).to.be.true;
+      expect(isChanged.calledWith(HTML, year, 'spa', dir)).to.be.true;
+      expect(save.calledWith(HTML, year, 'spa', dir)).to.be.false;
+    });
+  });
+});
+
+describe('SpMeasureScraper#run', () => {
+  let fakeScrape;
+
+  beforeEach(async () => {
+    await scrapeJob.deleteAllJobs(); 
+  });
+
+  context('when fetch fails', () => {
+    beforeEach(() => {
+      fakeScrape = sinon.stub(SpMeasureScraper, 'scrape').throws();
+    });
+
+    afterEach(() => {
+      fakeScrape.restore();
+    });
+
+    it('returns failed as the message', async () => {
+      let r = await SpMeasureScraper.run(year, session, dir);
+      expect(r).to.eql({ msg: 'failed', total: 0, updated: 0 });
+      expect(fakeScrape.calledWith(year, 'a', dir)).to.be.true;
+
+      r = await scrapeJob.selectOneJob(); 
+      expect(r.dataType).to.equal(3);
+      expect(r.status).to.equal(3);
+      expect(r.startedAt).to.be.above(0);
+      expect(r.completedAt).to.be.above(0);
+      expect(r.totalNumber).to.equal(0);
+      expect(r.updatedNumber).to.equal(0);
+      expect(r.updateNeeded).to.equal(0);
+    });
+  });
+
+  context('when the fetched page is updated', () => {
+    beforeEach(async () => {
+      fakeScrape = sinon.stub(SpMeasureScraper, 'scrape');
+      fakeScrape.returns({ msg: 'completed', total: 3, updated: 3 });
+    });
+
+    afterEach(() => {
+      fakeScrape.restore();
+    });
+
+    it('returns completed as the message', async () => {
+      let r = await SpMeasureScraper.run(year, session, dir);
+      expect(r).to.eql({ msg: 'completed', total: 3, updated: 3 });
+      expect(fakeScrape.calledWith(year, 'a', dir)).to.be.true;
+
+      r = await scrapeJob.selectOneJob(); 
+      const jobId = r.id;
+      expect(r.dataType).to.equal(3);
+      expect(r.status).to.equal(4);
+      expect(r.startedAt).to.be.above(0);
+      expect(r.completedAt).to.be.above(0);
+      expect(r.totalNumber).to.equal(3);
+      expect(r.updatedNumber).to.equal(3);
+      expect(r.updateNeeded).to.equal(1);
+    });
+  });
+
+  context('when the fetched page is not updated', () => {
+    beforeEach(async () => {
+      fakeScrape = sinon.stub(SpMeasureScraper, 'scrape');
+      fakeScrape.returns({ msg: 'skipped', total: 0, updated: 0 });
+    });
+
+    afterEach(() => {
+      fakeScrape.restore();
+    });
+
+    it('returns completed as the message', async () => {
+      let r = await SpMeasureScraper.run(year, session, dir);
+      expect(r).to.eql({ msg: 'skipped', total: 0, updated: 0 });
+      expect(fakeScrape.calledWith(year, 'a', dir)).to.be.true;
+
+      r = await scrapeJob.selectOneJob(); 
+      const jobId = r.id;
+      expect(r.dataType).to.equal(3);
+      expect(r.status).to.equal(2);
+      expect(r.startedAt).to.be.above(0);
+      expect(r.completedAt).to.be.above(0);
+      expect(r.totalNumber).to.equal(0);
+      expect(r.updatedNumber).to.equal(0);
+      expect(r.updateNeeded).to.equal(0);
     });
   });
 });
