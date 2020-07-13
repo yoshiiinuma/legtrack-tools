18dd61a5440920ece9ac1e0b6b3ea3725425b3b1
yoshiiinuma <iinumayoshiaki@gmail.com>
2020-07-12 23:09:35 -1000

    Change MeasurePusher to use Pusher

--- src/measure-pusher.js
+++ src/measure-pusher.js
@@ -1,86 +1,19 @@
 
-import RemoteMeasure from '../src/remote-measure.js';
-import PushJob from '../src/local-push-job.js';
-import ScrapeJob from '../src/local-scrape-job.js';
 import LocalMeasure from '../src/local-measure.js';
-import PushHelper from '../src/push-helper.js';
-import now from '../src/now.js';
-import ENUM from '../src/enum.js';
-import Logger from '../src/logger.js';
+import RemoteMeasure from '../src/remote-measure.js';
+import Pusher from '../src/pusher.js';
 
 const DEFAULT_ENV = 'development';
 const nodeEnv = process.env.NODE_ENV || DEFAULT_ENV;
 
-const dataType = ENUM.DataType.MEASURE;
-const STATUS = ENUM.JobStatus;
-
-/**
- * Pushes data updated after originTime to the DB
- */
-const push = async (originTime) => {
-  if (!originTime) {
-    Logger.error('MeasurePusher#push: Speficy Time to Select Data');
-    throw new Error('Speficy Time to Select Data');
-  }
-
-  const local = LocalMeasure.create(nodeEnv);
-  const data = await local.selectUpdatedAfter(originTime);
-  const size = data.length;
-  if (size === 0) {
-    Logger.info('MeasurePusher#push: No Unprocessed Data');
-    return { msg: 'No Unprocessed Data', skipped: true };
-  }
-
-  const remote = RemoteMeasure.create(nodeEnv);
-  const res = await remote.push(data);
-  const msg = `Push Completed ${size} Data`;
-  const rowsAffected = (res && res.rowsAffected)? res.rowsAffected[0] : null;
-  Logger.info('MeasurePusher#push: ' + msg);
-  return { msg, size, rowsAffected };
-};
-
-const run = async (year) => {
-  if (!year) {
-    Logger.error('MeasurePusher#run: Specify Year');
-    return Promise.reject('Specify Year');
-  }
-  const pushJob = PushJob.create(nodeEnv);
-  let job = null;
-  let pushJobId = null;
-
-  try {
-    const unprocessedScrapeJob = PushHelper.getUnprocessedScrapeJob(pushJob, dataType);
-    if (!unprocessedScrapeJob) {
-      Logger.info('MeasurePusher#run: No Unprocessed Scrape Jobs');
-      await pushJob.insert(dataType, 0, STATUS.skipped);
-      return { msg: 'No Unprocessed Scrape Jobs' };
-    }
-
-    job = await pushJob.insert(dataType, unprocessedScrapeJob.id);
-    pushJobId = job.lastInsertRowid;
-
-    const r = MeasurePusher.push(unprocessedScrapeJob.startedAt);
-    if (r.skipped) {
-      await pushJob.update(pushJobId, STATUS.skipped, 0, 0);
-    } else {
-      await pushJob.update(pushJobId, STATUS.completed, r.size, r.size);
-    }
-    return r;
-  } catch (e) {
-    const errmsg = e.toString();
-    Logger.error('MeasurePusher#run: ' + errmsg);
-    Logger.error(e.stack);
-    if (job && pushJobId) {
-      await pushJob.update(pushJobId, STATUS.failed, 0, 0);
-    } else {
-      await pushJob.insertError(dataType);
-    }
-    return { msg: errmsg, error: true };
-  }
+const run = async (env = nodeEnv) => {
+  const localModel = LocalMeasure.create(env);
+  const remoteModel = RemoteMeasure.create(env);
+  const pusher = Pusher.create('MEASURE', localModel, remoteModel, env);
+  return await pusher.run();
 };
 
 const MeasurePusher = {
-  push,
   run
 };
 
--- test/measure-pusher-test.js
+++ test/measure-pusher-test.js
@@ -2,235 +2,36 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 
-import Pusher from '../src/measure-pusher.js';
-import PushHelper from '../src/push-helper.js';
-import PushJob from '../src/local-push-job.js';
+import MeasurePusher from '../src/measure-pusher.js';
+import Pusher from '../src/pusher.js';
 import Measure from '../src/local-measure.js';
-import now from '../src/now.js';
-import sqliteHelper from './sqlite-helper.js';
-import sqlsrvHelper from './sqlsrv-helper.js';
+import RemoteMeasure from '../src/remote-measure.js';
+import LocalMeasure from '../src/local-measure.js';
 
-const data1 = sqliteHelper.generateBills([1, 2, 3, 4]);
-const data2 = sqliteHelper.generateBills([5, 6, 7]);
-
-const pushJob = PushJob.create('test');
-const bill = Measure.create('test');
-
-const ts1 = now();
-const ts2 = ts1 + 1001;
-
-describe('MeasurePusher#push', () => {
-  describe('with no time given', () => {
-    it('throws an exception', async () => {
-      try {
-        await Pusher.push(null);
-      }
-      catch (e) {
-        expect(e.toString()).to.equal('Error: Speficy Time to Select Data');
-      }
-    });
-  });
-
-  describe('with unprocessed data', () => {
-    let r;
-
-    beforeEach(async () => {
-      await sqlsrvHelper.deleteAllBills();
-      await bill.deleteAll();
-      r = await bill.bulkInsert(data1, ts1)
-      r = await bill.bulkInsert(data2, ts2)
-    });
-
-    it('push data updated after specified time', async () => {
-      r = await Pusher.push(ts2);
-      expect(r.msg).to.equal('Push Completed 3 Data');
-      expect(r.size).to.equal(3);
-      expect(r.rowsAffected).to.equal(3);
-
-      r = await sqlsrvHelper.countBills();
-      expect(r).to.equal(3);
-
-      r = await sqlsrvHelper.selectBills();
-      expect(r.map((e) => e.measureNumber)).to.eql([5, 6, 7]);
-      expect(r.map((e) => e.reportTitle)).to.eql(['REPORT5', 'REPORT6', 'REPORT7']);
-      expect(r.map((e) => e.measureTitle)).to.eql(['MEASURE5', 'MEASURE6', 'MEASURE7']);
-    });
-  });
-
-  describe('with no unprocessed data', () => {
-    let r;
-
-    beforeEach(async () => {
-      await sqlsrvHelper.deleteAllBills();
-      await bill.deleteAll();
-    });
-
-    it('push data updated after specified time', async () => {
-      r = await Pusher.push(ts1);
-      expect(r).to.eql({ msg: 'No Unprocessed Data', skipped: true });
-    });
-  });
-});
+const local = LocalMeasure.create('test');
+const remote = RemoteMeasure.create('test');
 
 describe('MeasurePusher#run', () => {
-  let r;
-  let stub1, stub2;
+  let stub1, stub2, stub3, stub4;
 
   beforeEach(() => {
-    pushJob.deleteAll(); 
+    stub1 = sinon.stub();
+    stub2 = sinon.stub(Pusher, 'create').returns({ run: stub1 });
+    stub3 = sinon.stub(LocalMeasure, 'create').returns(local);
+    stub4 = sinon.stub(RemoteMeasure, 'create').returns(remote);
   });
 
-  describe('with invalid arguments', () => {
-    it('throws an exception', async () => {
-      try {
-        await Pusher.run(null);
-      }
-      catch (e) {
-        expect(e).to.equal('Specify Year');
-      }
-    });
+  afterEach(() => {
+    stub2.restore();
+    stub3.restore();
+    stub4.restore();
   });
 
-  describe('with no unprocessed jobs', () => {
-    beforeEach(() => {
-      stub1 = sinon.stub(PushHelper, 'getUnprocessedScrapeJob').returns(null);
-    });
+  it('calls Pusher.cerate with correct arguments', () => {
+    MeasurePusher.run();
 
-    afterEach(() => {
-      stub1.restore();
-    });
-
-    it('returns error message', async () => {
-      r = await Pusher.run(2020);
-      expect(r.msg).to.equal('No Unprocessed Scrape Jobs');
-      expect(stub1.calledOnce).to.be.true;
-
-      r = await pushJob.selectOne();
-      expect(r.startedAt).to.be.above(0);
-      expect(r.scrapeJobId).to.equal(0);
-      expect(r.dataType).to.equal(1);
-      expect(r.status).to.equal(2);
-      expect(r.totalNumber).to.equal(0);
-      expect(r.updatedNumber).to.equal(0);
-    });
-  });
-
-  describe('when an exception throws', () => {
-    context('at getUnprocessedScrapeJob function', () => {
-      beforeEach(() => {
-        stub1 = sinon.stub(PushHelper, 'getUnprocessedScrapeJob').throws('Unexpected Error');
-      });
-
-      afterEach(() => {
-        stub1.restore();
-      });
-
-      it('returns error message', async () => {
-        r = await Pusher.run(2020);
-        expect(r.error).to.be.true;
-        expect(r.msg).to.equal('Unexpected Error');
-        expect(stub1.calledOnce).to.be.true;
-
-        r = await pushJob.selectOne();
-        expect(r.startedAt).to.be.above(0);
-        expect(r.scrapeJobId).to.equal(0);
-        expect(r.dataType).to.equal(1);
-        expect(r.status).to.equal(3);
-        expect(r.totalNumber).to.equal(0);
-        expect(r.updatedNumber).to.equal(0);
-      });
-    });
-
-    context('at push', () => {
-      const unprocessedJob = { id: 123, startedAt: ts2 };
-
-      beforeEach(() => {
-        stub1 = sinon.stub(PushHelper, 'getUnprocessedScrapeJob').returns(unprocessedJob);
-        stub2 = sinon.stub(Pusher, 'push').throws('Unexpected Error');
-      });
-
-      afterEach(() => {
-        stub1.restore();
-        stub2.restore();
-      });
-
-      it('returns error message', async () => {
-        r = await Pusher.run(2020);
-        expect(r.error).to.be.true;
-        expect(r.msg).to.equal('Unexpected Error');
-        expect(stub1.calledOnce).to.be.true;
-        expect(stub2.calledWith(ts2)).to.be.true;
-
-        r = await pushJob.selectOne();
-        expect(r.startedAt).to.be.above(0);
-        expect(r.scrapeJobId).to.equal(123);
-        expect(r.dataType).to.equal(1);
-        expect(r.status).to.equal(3);
-        expect(r.totalNumber).to.equal(0);
-        expect(r.updatedNumber).to.equal(0);
-      });
-    });
-  });
-
-  describe('with no unprocessed data', () => {
-    const unprocessedJob = { id: 123, startedAt: ts2 };
-    const pushRslt = { msg: 'No Unprocessed Data', skipped: true };
-
-    beforeEach(() => {
-      stub1 = sinon.stub(PushHelper, 'getUnprocessedScrapeJob').returns(unprocessedJob);
-      stub2 = sinon.stub(Pusher, 'push').returns(pushRslt);
-    });
-
-    afterEach(() => {
-      stub1.restore();
-      stub2.restore();
-    });
-
-    it('returns error message', async () => {
-      r = await Pusher.run(2020);
-      expect(r.skipped).to.be.true;
-      expect(r.msg).to.equal('No Unprocessed Data');
-      expect(stub1.calledOnce).to.be.true;
-      expect(stub2.calledWith(ts2)).to.be.true;
-
-      r = await pushJob.selectOne();
-      expect(r.startedAt).to.be.above(0);
-      expect(r.completedAt).to.be.above(0);
-      expect(r.scrapeJobId).to.equal(123);
-      expect(r.status).to.equal(2);
-      expect(r.totalNumber).to.equal(0);
-      expect(r.updatedNumber).to.equal(0);
-    });
-  });
-
-  describe('with unprocessed data', () => {
-    const unprocessedJob = { id: 123, startedAt: ts2 };
-    const pushRslt = { msg: 'Push Completed 3 Data', size: 3, rowAffected: 3 };
-
-    beforeEach(() => {
-      stub1 = sinon.stub(PushHelper, 'getUnprocessedScrapeJob').returns(unprocessedJob);
-      stub2 = sinon.stub(Pusher, 'push').returns(pushRslt);
-    });
-
-    afterEach(() => {
-      stub1.restore();
-      stub2.restore();
-    });
-
-    it('returns error message', async () => {
-      r = await Pusher.run(2020);
-      expect(r).to.eql(pushRslt);
-      expect(stub1.calledOnce).to.be.true;
-      expect(stub2.calledWith(ts2)).to.be.true;
-
-      r = await pushJob.selectOne();
-      expect(r.startedAt).to.be.above(0);
-      expect(r.completedAt).to.be.above(0);
-      expect(r.scrapeJobId).to.equal(123);
-      expect(r.status).to.equal(4);
-      expect(r.totalNumber).to.equal(3);
-      expect(r.updatedNumber).to.equal(3);
-    });
+    expect(stub2.calledWith('MEASURE', local, remote, 'test')).to.be.true;
+    expect(stub1.called).to.be.true;
   });
 });
 
